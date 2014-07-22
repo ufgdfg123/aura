47ecef09418e1612d91926fd5d112d5e6d3a50d1
exec <exec@mabinoger.de>
2014-07-22 11:26:18 +0200

    Added test test for Packet
    My first C# unit test *_* Basically added the three methods that I
    usually use to test Packet after I rewrote it.

---
+++ tests/Shared.Tests/Network/Packet.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Aura.Shared.Network;
+using Xunit;
+
+namespace Aura.Shared.Tests.Network
+{
+	public class PacketTests
+	{
+		[Fact]
+		public void BuildingNewPacket()
+		{
+			var packet = new Packet(0x1020, 0x3040506070809010);
+			packet.PutByte(1);
+			packet.PutShort(2);
+			packet.PutInt(3);
+			packet.PutLong(4);
+			packet.PutFloat(5.6f);
+			packet.PutString("test");
+			packet.PutBin(new byte[] { 1, 2, 3, 4, 5, 6, 8, 9 });
+
+			var built = packet.Build();
+
+			Assert.Equal(built, new byte[] { 0x00, 0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0x10, 0x2B, 0x07, 0x00, 0x01, 0x01, 0x02, 0x00, 0x02, 0x03, 0x00, 0x00, 0x00, 0x03, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x05, 0x33, 0x33, 0xB3, 0x40, 0x06, 0x00, 0x05, 0x74, 0x65, 0x73, 0x74, 0x00, 0x07, 0x00, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x08, 0x09 });
+		}
+
+		[Fact]
+		public void ReadingPacketFromBuffer()
+		{
+			var buffer = new byte[] { 0x00, 0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0x10, 0x2B, 0x07, 0x00, 0x01, 0x01, 0x02, 0x00, 0x02, 0x03, 0x00, 0x00, 0x00, 0x03, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x05, 0x33, 0x33, 0xB3, 0x40, 0x06, 0x00, 0x05, 0x74, 0x65, 0x73, 0x74, 0x00, 0x07, 0x00, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x08, 0x09 };
+
+			var packet = new Packet(buffer, 0);
+
+			Assert.Equal(1, packet.GetByte());
+			Assert.Equal(2, packet.GetShort());
+			Assert.Equal(3, packet.GetInt());
+			Assert.Equal(4, packet.GetLong());
+			Assert.Equal(5.6f, packet.GetFloat());
+			Assert.Equal("test", packet.GetString());
+			Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6, 8, 9 }, packet.GetBin());
+		}
+
+		[Fact]
+		public void ReadingPacketFromPacket()
+		{
+			var packetW = new Packet(0x1020, 0x3040506070809010);
+			packetW.PutByte(1);
+			packetW.PutShort(2);
+			packetW.PutInt(3);
+			packetW.PutLong(4);
+			packetW.PutFloat(5.6f);
+			packetW.PutString("test");
+			packetW.PutBin(new byte[] { 1, 2, 3, 4, 5, 6, 8, 9 });
+
+			var buffer = packetW.Build();
+
+			var packetR = new Packet(buffer, 0);
+
+			Assert.Equal(1, packetR.GetByte());
+			Assert.Equal(2, packetR.GetShort());
+			Assert.Equal(3, packetR.GetInt());
+			Assert.Equal(4, packetR.GetLong());
+			Assert.Equal(5.6f, packetR.GetFloat());
+			Assert.Equal("test", packetR.GetString());
+			Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6, 8, 9 }, packetR.GetBin());
+		}
+	}
+}
